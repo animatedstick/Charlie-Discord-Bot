0ef3d6207f17b7b11eb6656baa78c039a25f19c1
animatedstick <42507233+animatedstick@users.noreply.github.com>
2019-04-19 17:35:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ bot = commands.Bot(command_prefix=bot_prefix)
 bot.remove_command("help")
 
 
-
 #startup_extensions = [
 #   'cogs.info',
 #    'cogs.mod'
@@ -23,29 +22,6 @@ async def on_ready():
     print("ID: {}".format(bot.user.id))
     print("Users: {}".format(len(set(bot.get_all_members()))))
     await bot.change_presence(game=discord.Game(name="c!help | {} Users ".format(len(set(bot.get_all_members()))),type=3))
-
-
-@bot.event
-async def on_message(message):
-        if message.author.bot:
-            return
-
-        elif message.content.startswith("c!say"):
-            if message.author.id == "429118689367949322":
-                text = message.content[len('<tell'):].strip()
-                if "efnejifn" in text:
-                    return
-                elif "discord." in text:
-                    return
-                elif "@here" in text:
-                    return
-                elif "@everyone" in text:
-                    return
-                else:
-                    await bot.send_message(message.channel ,"{}".format(text))
-                    await bot.delete_message(message)
-            else:
-                return
 
 
 
