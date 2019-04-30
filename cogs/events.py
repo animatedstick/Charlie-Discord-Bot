2f9e2d48e8477850bf080e2c442921ee62f2973c
animatedstick <42507233+animatedstick@users.noreply.github.com>
2019-04-30 22:01:48 +0530

    Update events.py

--- cogs/events.py
+++ cogs/events.py
@@ -25,22 +25,22 @@ class Events(commands.Cog, name='Events'):
     @commands.Cog.listener()
     async def on_member_join(self,member):
         server = member.guild
-        if server.id == "554641659888140308":
+        if server.id == 554641659888140308:
             wlc = self.bot.get_channel(557774869719416844)
-            embed=discord.Embed(title="Welcome to the Crew!", url="https://discord.gg/9SVUNya", description=f"Welcome to the the AnimatedStick Crew Discord Server,\n {member.name} :wave:\n\nMake sure to check out the AnimatedStick YouTube Channel\nSubscribe & Support : [Channel Link](https://www.youtube.com/channel/UC0bZpIWLn_YEjkyTyvAadtQ?view_as=subscriber)\n`Have Great Time At the AnimatedStick Server`", color=0xfbb420)
+            embed=discord.Embed(title="Welcome!", url="https://discord.gg/9SVUNya", description=f"Welcome to the the AnimatedStick Crew Discord Server,\n **{member.name}** :wave:\n\nMake sure to check out the AnimatedStick YouTube Channel\nSubscribe & Support : [Channel Link](https://www.youtube.com/channel/UC0bZpIWLn_YEjkyTyvAadtQ?view_as=subscriber)\n`Have Great Time At the AnimatedStick Server`", color=0xfbb420)
             embed.set_author(name=member.name, icon_url=str(member.avatar_url))
             embed.set_thumbnail(url=str(member.avatar_url))
             embed.timestamp = datetime.datetime.utcnow()
-            embed.set_footer(text=f"the AnimatedStick Crew | Welcome {member.name}!",icon_url=str(server.icon_url))
+            embed.set_footer(text=f"the AnimatedStick Crew",icon_url=str(server.icon_url))
             await wlc.send(embed=embed)
 
-        elif server.id == "572346295830970370":
+        elif server.id == 572346295830970370:
             wlc = self.bot.get_channel(572761367933747200)
-            embed=discord.Embed(title="Charlie Support Server!!", url="https://discord.gg/9SVUNya", description=f"Welcome to the Charlie Support Server,\n {member.name} :wave:\n\nMake sure to vote Charlie at the Discord Bot List\nVote & Support : [Link](https://www.youtube.com/channel/UC0bZpIWLn_YEjkyTyvAadtQ?view_as=subscriber)\n`Have Great Time At the Charlie Support Server`", color=0xfe8601)
+            embed=discord.Embed(title="Charlie Support Server", url="https://discord.gg/9SVUNya", description=f"Welcome to the Charlie Support Server,\n **{member.name}** :wave:\n\nMake sure to vote Charlie at the Discord Bot List\nVote & Support : [Link](https://www.youtube.com/channel/UC0bZpIWLn_YEjkyTyvAadtQ?view_as=subscriber)\n`Have Great Time At the Charlie Support Server`", color=0xfe8601)
             embed.set_author(name=member.name, icon_url=str(member.avatar_url))
             embed.set_thumbnail(url=str(member.avatar_url))
             embed.timestamp = datetime.datetime.utcnow()
-            embed.set_footer(text=f"the AnimatedStick Crew | Welcome {member.name}!",icon_url=str(server.icon_url))
+            embed.set_footer(text=f"Charlie Support Server",icon_url=str(server.icon_url))
             await wlc.send(embed=embed)
         else:
             return
